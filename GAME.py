49310e9ed15122ff42f974618c7514fd0200d095
KubaBanszel <kuba.banszel@gmail.com>
2023-04-24 17:21:40 +0200

    Create GAME.py

---
+++ GAME.py
@@ -0,0 +1,24 @@
+import kostka
+import pocetHracu
+"""
+# Define the dimensions of the board
+rows = 8
+cols = 8
+
+# Create the board as a 2D list
+board = []
+for i in range(rows):
+    row = []
+    for j in range(cols):
+        # If the row number plus the column number is even, the square is black
+        if (i+j) % 2 == 0:
+            row.append("██")
+        # Otherwise, the square is white
+        else:
+            row.append("  ")
+    board.append(row)
+
+# Print the board
+for row in board:
+    print(''.join(row))
+"""
