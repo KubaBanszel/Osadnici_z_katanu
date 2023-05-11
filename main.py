6d1cbd9942b1e7cf795795b0bdf671b693fe1112
stof20 <129372748+stof20@users.noreply.github.com>
2023-05-11 10:37:52 +0200

    main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import GAME
 import kostka
 import pocetHracu
 rules = open("pravidla.txt", "r")
-print(f"Výtej u pthon hry Osadníci z Katanu,\n Pro zobrazení pravidel zavolej funkcy pravidla()\n pro začátek hry zavolej funkcy hra()")
+print(f"Výtej u python hry Osadníci z Katanu,\n Pro zobrazení pravidel zavolej funkcy pravidla()\n pro začátek hry zavolej funkcy hra()")
 
 def pravidla():
     text = rules.readlines()
