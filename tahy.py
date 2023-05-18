aed140d592b847ec8351a1454a111cd41368098d
KubaBanszel <129372533+KubaBanszel@users.noreply.github.com>
2023-05-18 11:06:06 +0200

    Update tahy.py

--- tahy.py
+++ tahy.py
@@ -1,8 +1,15 @@
 import kostka
 import pohyb_hrace
+import GAME
 
 poleHrace = []
 
 if hrac_x and hrac_y == 1:
-    poleHrace.append(1)
-    print(poleHrace)
+    if penize >= 5:
+        odpoved = input("Chceš si zde koupit budovu? Ano = a; Ne = n:")
+        if odpoved == a:
+            poleHrace.append(1)
+            penize -= 5
+        else:
+            pohyb()
+            
