fc5439f9e90eaa472fb54055ecc284450397f7fd
htjozmkaea <129585288+htjozmkaea@users.noreply.github.com>
2023-05-18 10:42:23 +0200

    kostka.py

--- kostka.py
+++ kostka.py
@@ -1,6 +1,4 @@
 import random
 
 def kostkaFunkce():
-    kostka1 = random.randint(1,6)
-    kostka2 = random.randint(1,6)
-    print(f"{kostka1} + {kostka2} celkem {kostka1+kostka2}")
+    kostka = random.randint(1,5)
