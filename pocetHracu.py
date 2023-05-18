02d67925dbff242061dba3fd714ce95effadf95e
htjozmkaea <129585288+htjozmkaea@users.noreply.github.com>
2023-05-18 10:07:18 +0200

    Delete pocetHracu.py

--- pocetHracu.py
+++
@@ -1,8 +0,0 @@
-def pocitaniHracu():
-    pocetHracu = int(input("Zadej pocet hracu od 2 do 4: "))
-
-    if pocetHracu >= 5 or pocetHracu <= 1:
-        print("Špatně zadaný počet hráčů, počet hráčů musí být od 2 do 4")
-        pocitaniHracu()
-
-pocitaniHracu()
