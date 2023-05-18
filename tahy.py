1489d04d989445d11fb7027e5fca5cdbb2d5f729
KubaBanszel <129372533+KubaBanszel@users.noreply.github.com>
2023-05-18 11:17:15 +0200

    Update tahy.py

--- tahy.py
+++ tahy.py
@@ -7,7 +7,7 @@ poleHrace = []
 if hrac_x and hrac_y == 1:
     if penize >= 5:
         odpoved = input("Chceš si zde koupit budovu? Ano = a; Ne = n:")
-        if odpoved == a:
+        if odpoved == "a":
             poleHrace.append(1)
             penize -= 5
         else:
