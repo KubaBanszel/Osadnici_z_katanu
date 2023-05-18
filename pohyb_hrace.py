af3b0b922db634499afee951b49552c0980b524a
KubaBanszel <129372533+KubaBanszel@users.noreply.github.com>
2023-05-18 11:07:13 +0200

    Update pohyb_hrace.py

--- pohyb_hrace.py
+++ pohyb_hrace.py
@@ -1,33 +1,33 @@
 MAP_WIDTH = 5
 MAP_HEIGHT = 5
-
+tah = 1
 
 hrac_x = 0
 hrac_y = 0
-tah = 1
 
-while tah <21:
-    print(f"Jsi na tahu číslo {tah}")
-    tah += 1
-    for y in range(MAP_HEIGHT):
-        for x in range(MAP_WIDTH):
-            if x == hrac_x and y == hrac_y:
-                print("H", end=" ")  
-            else:
-                print(".", end=" ")  
-        print()  
+def pohyb():
+    while tah <21:
+        print(f"Jsi na tahu číslo {tah}")
+        tah += 1
+        for y in range(MAP_HEIGHT):
+            for x in range(MAP_WIDTH):
+                if x == hrac_x and y == hrac_y:
+                    print("H", end=" ")  
+                else:
+                    print(".", end=" ")  
+            print()  
 
-    
-    smer = input("Zadej smer (w/a/s/d): ")
 
-    
-    if smer == "w" and hrac_y > 0:
-        hrac_y -= 1 
-    elif smer == "a" and hrac_x > 0:
-        hrac_x -= 1  
-    elif smer == "s" and hrac_y < MAP_HEIGHT - 1:
-        hrac_y += 1  
-    elif smer == "d" and hrac_x < MAP_WIDTH - 1:
-        hrac_x += 1 
+        smer = input("Zadej smer (w/a/s/d): ")
+
+
+        if smer == "w" and hrac_y > 0:
+            hrac_y -= 1 
+        elif smer == "a" and hrac_x > 0:
+            hrac_x -= 1  
+        elif smer == "s" and hrac_y < MAP_HEIGHT - 1:
+            hrac_y += 1  
+        elif smer == "d" and hrac_x < MAP_WIDTH - 1:
+            hrac_x += 1 
 
 
